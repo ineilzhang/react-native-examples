8ea9cf6607a6e3b5dc85ae2f9b9da84759b3e9ff
ineilzhang <zyf13682501215@gmail.com>
2017-09-19 17:13:59 +0800

    FlatList and SectionList abouts

--- index.ios.js
+++ index.ios.js
@@ -1,90 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// import React, { Component } from 'react';
+// import { AppRegistry, FlatList, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+//
+
+//The FlatList component displays a scrolling list of changing, but similarly structured, data.
+//FlatList works well for long lists of data, where the number of items might change over time.
+//Unlike the more generic ScrollView, the FlatList only renders elements that are currently showing on the screen,
+//not all the elements at once.
+
+// export default class FlatListBasics extends Component {
+//   render() {
+/*
+ The FlatList component requires two props: data and renderItem.
+ data is the source of information for the list.
+ renderItem takes one item from the source and returns a formatted component to render.
+*/
+//     return (
+//         <View style={styles.container}>
+//   <FlatList
+//     data={[
+//       {key: 'Deving'},
+//       {key: 'Jacksonwt'},
+//       {key: 'Jamestew'},
+//       {key: 'Joeltwe'},
+//       {key: 'Johntew'},
+//       {key: 'Jillianwtee'},
+//       {key: 'Jimmywer'},
+//       {key: 'Julierete'},
+//       {key: 'Jackson1'},
+//       {key: 'Jameseree'},
+//       {key: 'Joel1'},
+//       {key: 'John2'},
+//       {key: 'Jillian32'},
+//       {key: 'Jimmy23'},
+//       {key: 'Julie24'},
+//       {key: 'Jackson43'},
+//       {key: 'James234'},
+//       {key: 'Joel35'},
+//       {key: 'John235'},
+//       {key: 'Jillian535'},
+//       {key: 'Jimmy23dfdfs'},
+//       {key: 'Julie35'},
+//       {key: 'Jackson235'},
+//       {key: 'James53'},
+//       {key: 'Joel634'},
+//       {key: 'John346'},
+//       {key: 'Jillian646'},
+//       {key: 'Jimmy64'},
+//       {key: 'Julie646'},
+//   ]}
+//     renderItem={
+//       ({item}) =>
+//           <TouchableOpacity onPress={this._onPressButton}>
+//             <View style={styles.button}>
+//               <Text style={styles.buttonText}>{item.key}</Text>
+//             </View>
+//           </TouchableOpacity>
+//     }
+//   />
+//   </View>
+//   );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 22
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44,
+//   },
+//   button: {
+//     marginBottom: 2,
+//     width: 260,
+//     alignItems: 'center',
+//     backgroundColor: '#2196F3'
+//   },
+//   buttonText: {
+//     padding: 20,
+//     color: 'white'
+//   }
+// })
+//
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
+
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
 
-/*
- 在ES5里，要导出一个类给别的模块用，一般通过module.exports来导出
-
- //ES5
- var MyComponent = React.createClass({
- ...
- });
-
- module.exports = MyComponent;
-
- 在ES6里，通常用export default来实现相同的功能：
-
- //ES6
- export default class MyComponent extends Component{
- ...
- }
-
- 引用的时候也类似：
-
- //ES5
- var MyComponent = require('./MyComponent');
-
- //ES6
- import MyComponent from './MyComponent';
- 注意导入和导出的写法必须配套，不能混用！
- */
-
-export default class AwesomeProject extends Component {
-
-  /*
-   给组件定义方法
-
-   给组件定义方法不再用 名字: function() 的写法，而是直接用 方法名() ，在方法的最后也不能有逗号了。
-
-   //ES5
-   var Photo = React.createClass({
-   componentWillMount: function(){
-
-   },
-   render: function() {
-   return (
-   <Image source={this.props.source} />
-   );
-   },
-   });
-   //ES6
-   class Photo extends React.Component {
-   componentWillMount() {
-
-   }
-   render() {
-   return (
-   <Image source={this.props.source} />
-   );
-   }
-   }
-   */
-
-
+export default class SectionListBasics extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <SectionList
+              sections={[
+                {title: 'D', data: ['Devin']},
+                {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+              ]}
+              renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+              renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          />
+        </View>
     );
   }
 }
@@ -92,20 +111,23 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 22
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-});
+})
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
