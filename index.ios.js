b55adf5eb53ce1b9eff67ca61522e51a8fd64e48
ineilzhang <zyf13682501215@gmail.com>
2017-09-19 20:19:32 +0800

    Networing abouts

--- index.ios.js
+++ index.ios.js
@@ -9,82 +9,60 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
 } from 'react-native';
 
-/*
- 在ES5里，要导出一个类给别的模块用，一般通过module.exports来导出
-
- //ES5
- var MyComponent = React.createClass({
- ...
- });
-
- module.exports = MyComponent;
-
- 在ES6里，通常用export default来实现相同的功能：
-
- //ES6
- export default class MyComponent extends Component{
- ...
- }
-
- 引用的时候也类似：
-
- //ES5
- var MyComponent = require('./MyComponent');
-
- //ES6
- import MyComponent from './MyComponent';
- 注意导入和导出的写法必须配套，不能混用！
- */
-
 export default class AwesomeProject extends Component {
-
-  /*
-   给组件定义方法
-
-   给组件定义方法不再用 名字: function() 的写法，而是直接用 方法名() ，在方法的最后也不能有逗号了。
-
-   //ES5
-   var Photo = React.createClass({
-   componentWillMount: function(){
-
-   },
-   render: function() {
-   return (
-   <Image source={this.props.source} />
-   );
-   },
-   });
-   //ES6
-   class Photo extends React.Component {
-   componentWillMount() {
-
-   }
-   render() {
-   return (
-   <Image source={this.props.source} />
-   );
-   }
-   }
-   */
-
+  constructor(props){
+    super(props);
+    this.state = {responseJsonText: ''};
+  }
+  _onPressButton(){
+    return this._getMoviesFromApiAsync;
+  }
+  // 在ES5下，React.createClass会把所有的方法都bind一遍，这样可以提交到任意的地方作为回调函数，而this不会变化。
+  // 在ES6下，你需要通过bind来绑定this引用，或者使用箭头函数（它会绑定当前scope的this引用）来调用
+  _getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log("neil:"+responseJson.title);
+          this.setState({
+            responseJsonText: responseJson.title,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+  async _getTitleFromApi() {
+    try {
+      let response = await fetch('https://facebook.github.io/react-native/movies.json');
+      let responseJson = await response.json();
+      this.setState({
+        responseJsonText: responseJson.title,
+      });
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      // 箭头函数实际上是在这里定义了一个临时的函数，
+      // 箭头函数的箭头=>之前是一个空括号、单个的参数名、或用括号括起的多个参数名，
+      // 而箭头之后可以是一个表达式（作为函数的返回值），或者是用花括号括起的函数体（需要自行通过return来返回值，否则返回的是undefined）。
+      // 需要注意的是，不论是bind还是箭头函数，每次被执行都返回的是一个新的函数引用，
+      // 因此如果你还需要函数的引用去做一些别的事情（譬如卸载监听器），那么你必须自己保存这个引用.
+        <TouchableHighlight
+          onPress={this._getTitleFromApi.bind(this)}
+          // onPress={() => this._getMoviesFromApiAsync()}
+          underlayColor="white" style={{paddingTop: 22}}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>{!this.state.responseJsonText ? "Press Me" : this.state.responseJsonText}</Text>
+          </View>
+        </TouchableHighlight>
     );
   }
 }
@@ -105,6 +83,22 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  },
+  jsonText: {
+    paddingTop: 140,
+    color: 'red',
+    backgroundColor: '#F5FCFF',
+
   },
 });
 
