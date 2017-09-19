35d7727f0bcdbfc5bbcba59c65016ce04178de8d
ineilzhang <zyf13682501215@gmail.com>
2017-09-19 16:36:24 +0800

    ScrollView abouts

--- index.ios.js
+++ index.ios.js
@@ -1,111 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, ScrollView, Image, Text } from 'react-native';
 
-/*
- 在ES5里，要导出一个类给别的模块用，一般通过module.exports来导出
-
- //ES5
- var MyComponent = React.createClass({
- ...
- });
-
- module.exports = MyComponent;
-
- 在ES6里，通常用export default来实现相同的功能：
-
- //ES6
- export default class MyComponent extends Component{
- ...
- }
-
- 引用的时候也类似：
-
- //ES5
- var MyComponent = require('./MyComponent');
-
- //ES6
- import MyComponent from './MyComponent';
- 注意导入和导出的写法必须配套，不能混用！
- */
-
-export default class AwesomeProject extends Component {
-
-  /*
-   给组件定义方法
-
-   给组件定义方法不再用 名字: function() 的写法，而是直接用 方法名() ，在方法的最后也不能有逗号了。
-
-   //ES5
-   var Photo = React.createClass({
-   componentWillMount: function(){
-
-   },
-   render: function() {
-   return (
-   <Image source={this.props.source} />
-   );
-   },
-   });
-   //ES6
-   class Photo extends React.Component {
-   componentWillMount() {
-
-   }
-   render() {
-   return (
-   <Image source={this.props.source} />
-   );
-   }
-   }
-   */
-
-
+export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        //Set up the maximumZoomScale and minimumZoomScale props
+        //and your user will be able to use pinch and expand gestures to zoom in and out.
+        <ScrollView maximumZoomScale={1} minimumZoomScale={0.5}>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>If you like</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>Scrolling down</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>What's the best</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>Framework around?</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:80}}>React Native</Text>
+        </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+// skip these lines if using Create React Native App
+AppRegistry.registerComponent(
+    'AwesomeProject',
+    () => IScrolledDownAndWhatHappenedNextShockedMe);
