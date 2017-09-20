187b62e50147ff1ef769b3d2852639b45c36c5e2
ineilzhang <zyf13682501215@gmail.com>
2017-09-20 18:05:39 +0800

    stackNavigator and TabNavigator abouts

--- index.ios.js
+++ index.ios.js
@@ -1,105 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
-  TouchableHighlight,
+  Button,
 } from 'react-native';
+import {
+  StackNavigator,
+  TabNavigator,
+} from 'react-navigation';
 
-export default class AwesomeProject extends Component {
-  constructor(props){
-    super(props);
-    this.state = {responseJsonText: ''};
-  }
-  _onPressButton(){
-    return this._getMoviesFromApiAsync;
-  }
-  // 在ES5下，React.createClass会把所有的方法都bind一遍，这样可以提交到任意的地方作为回调函数，而this不会变化。
-  // 在ES6下，你需要通过bind来绑定this引用，或者使用箭头函数（它会绑定当前scope的this引用）来调用
-  _getMoviesFromApiAsync() {
-    return fetch('https://facebook.github.io/react-native/movies.json')
-        .then((response) => response.json())
-        .then((responseJson) => {
-          console.log("neil:"+responseJson.title);
-          this.setState({
-            responseJsonText: responseJson.title,
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-  }
-  async _getTitleFromApi() {
-    try {
-      let response = await fetch('https://facebook.github.io/react-native/movies.json');
-      let responseJson = await response.json();
-      this.setState({
-        responseJsonText: responseJson.title,
-      });
-    } catch(error) {
-      console.error(error);
-    }
-  }
-
+class ChatScreen extends React.Component {
+  // Nav options can be defined as a function of the screen's props:
+  static navigationOptions = ({navigation}) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
   render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
     return (
-      // 箭头函数实际上是在这里定义了一个临时的函数，
-      // 箭头函数的箭头=>之前是一个空括号、单个的参数名、或用括号括起的多个参数名，
-      // 而箭头之后可以是一个表达式（作为函数的返回值），或者是用花括号括起的函数体（需要自行通过return来返回值，否则返回的是undefined）。
-      // 需要注意的是，不论是bind还是箭头函数，每次被执行都返回的是一个新的函数引用，
-      // 因此如果你还需要函数的引用去做一些别的事情（譬如卸载监听器），那么你必须自己保存这个引用.
-        <TouchableHighlight
-          onPress={this._getTitleFromApi.bind(this)}
-          // onPress={() => this._getMoviesFromApiAsync()}
-          underlayColor="white" style={{paddingTop: 22}}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>{!this.state.responseJsonText ? "Press Me" : this.state.responseJsonText}</Text>
-          </View>
-        </TouchableHighlight>
+      <View>
+        <Text>Chat with { params.user }</Text>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  },
-  jsonText: {
-    paddingTop: 140,
-    color: 'red',
-    backgroundColor: '#F5FCFF',
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat',{ user: 'I am home.'})}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
 
-  },
+class RecentChatsScreen extends React.Component {
+  render() {
+    return <Button
+      onPress={() => this.props.navigation.navigate('Chat', { user: 'Lucy' })}
+      title="Chat with Lucy"
+    />
+  }
+}
+
+class AllContactsScreen extends React.Component {
+  render() {
+    return <Button
+      onPress={() => this.props.navigation.navigate('Chat', { user: 'Lucy' })}
+      title="Chat with Lucy"
+    />
+  }
+}
+
+
+// export default class NavigatorWrappingScreen extends React.Component {
+//   render() {
+//     console.log("logggggggg");
+//     return (
+//       <View>
+//         <MainScreenNavigator navigation={this.props.navigation}/>
+//       </View>
+//     );
+//   }
+// }
+// NavigatorWrappingScreen.router = MainScreenNavigator.router;
+
+
+
+const MainScreenNavigator = TabNavigator({
+  Recent: { screen: RecentChatsScreen },
+  All: { screen: AllContactsScreen },
+});
+const SimpleApp = StackNavigator({
+  Home: {
+    screen: MainScreenNavigator,
+    navigationOptions: {
+      title: 'My Chats',
+    },
+   },
+  Chat: { screen: ChatScreen },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+// if you are using create-react-native-app you don't need this line
+AppRegistry.registerComponent('AwesomeProject', () => SimpleApp);
